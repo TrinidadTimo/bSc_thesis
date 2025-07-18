c104b71fc0135495eb994188153b749854851356
TrinidadTimo <timo.trinidad@gmail.com>
2025-07-18 13:55:41 +0200

    Added code for mstmip, fluxcom and p-mod plots.

--- analysis/distribution_plot.R
+++ analysis/distribution_plot.R
@@ -19,6 +19,7 @@ for (file in trendy_gpp_files) {
   df_trendy_gpp <- merge(df_trendy_gpp, mod, by = "year", all.x = TRUE)
 }
 
+df_trendy_gpp %>% filter(year %in% 1982:2011) %>% select(-`ISBA-CTRIP`) %>% write.csv(file = here("data/trendy_S3_gpp_glob.csv"))
 
 df_trendy_gpp_long <- df_trendy_gpp |>
   pivot_longer(cols= -year,
@@ -47,9 +48,10 @@ for(i in 1:2) {
       strip.text = element_text(face = "bold")
     ) +
     facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
-  ggsave(sprintf(here("analysis/distribution_trendy_gpp_%01d.png"), i), plot = p_trendy_gpp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+  ggsave(sprintf(here("figures/distribution_trendy_gpp_%01d.png"), i), plot = p_trendy_gpp, width = 8.27, height = 11.69, units= "in", dpi = 300)
 
 }
+
 
 ## NBP:
 trendy_nbp_files <- list.files(path= here("../data/trendy/S3/ann_glob_absolute/R/as_txt"), pattern = "_nbp_.*\\.txt$")
@@ -64,6 +66,8 @@ for (file in trendy_nbp_files) {
   mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
   df_trendy_nbp <- merge(df_trendy_nbp, mod, by = "year", all.x = TRUE)
 }
+
+df_trendy_nbp %>% filter(year %in% 1982:2011) %>% select(-`ISBA-CTRIP`) %>% write.csv(file = here("data/trendy_S3_nbp_glob.csv"))
 
 df_trendy_nbp_long <- df_trendy_nbp |>
   pivot_longer(cols= -year,
@@ -92,10 +96,9 @@ for(i in 1:2) {
       strip.text = element_text(face = "bold")
     ) +
     facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
-  ggsave(sprintf(here("analysis/distribution_trendy_nbp_%01d.png"), i), plot = p_trendy_nbp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+  ggsave(sprintf(here("figures/distribution_trendy_nbp_%01d.png"), i), plot = p_trendy_nbp, width = 8.27, height = 11.69, units= "in", dpi = 300)
 
 }
-
 
 # CMIP:
 ## GPP:
@@ -111,6 +114,9 @@ for (file in cmip_gpp_files) {
   mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
   df_cmip_gpp <- merge(df_cmip_gpp, mod, by = "year", all.x = TRUE)
 }
+
+df_cmip_gpp %>% filter(year %in% 1982:2011) %>% write.csv(file = here("data/cmip_gpp_glob.csv"))
+
 
 df_cmip_gpp_long <- df_cmip_gpp |>
   pivot_longer(cols= -year,
@@ -139,7 +145,7 @@ for(i in 1:3) {
       strip.text = element_text(face = "bold")
     ) +
     facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
-  ggsave(sprintf(here("analysis/distribution_cmip_gpp_%01d.png"), i), plot = p_cmip_gpp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+  ggsave(sprintf(here("figures/distribution_cmip_gpp_%01d.png"), i), plot = p_cmip_gpp, width = 8.27, height = 11.69, units= "in", dpi = 300)
 
 }
 
@@ -157,6 +163,9 @@ for (file in cmip_nbp_files) {
   mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
   df_cmip_nbp <- merge(df_cmip_nbp, mod, by = "year", all.x = TRUE)
 }
+
+df_cmip_nbp %>% filter(year %in% 1982:2011) %>% write.csv(file = here("data/cmip_nbp_glob.csv"))
+
 
 df_cmip_nbp_long <- df_cmip_nbp |>
   pivot_longer(cols= -year,
@@ -185,7 +194,241 @@ for(i in 1:3) {
       strip.text = element_text(face = "bold")
     ) +
     facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
-  ggsave(sprintf(here("analysis/distribution_cmip_nbp_%01d.png"), i), plot = p_cmip_nbp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+  ggsave(sprintf(here("figures/distribution_cmip_nbp_%01d.png"), i), plot = p_cmip_nbp, width = 8.27, height = 11.69, units= "in", dpi = 300)
 
 }
 
+
+
+# MsTMIP:
+## SG1
+### GPP:
+mstmip_sg1_gpp_files <- list.files(path= here("../data/mstmip/SG1/ann_glob/as_txt/"), pattern = "_gpp_.*\\.txt$")
+
+df_mstmip_sg1_gpp = data.frame("year" = 1982:2010)
+
+for (file in mstmip_sg1_gpp_files) {
+  mod_name <- sub("_gpp_ann_glob.txt", "", file)
+  mod <- read.table(paste("../data/mstmip/SG1/ann_glob/as_txt/", file, sep= "/"), quote= "\"")
+  colnames(mod) <- c("year", mod_name)
+
+  mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
+  df_mstmip_sg1_gpp <- merge(df_mstmip_sg1_gpp, mod, by = "year", all.x = TRUE)
+}
+
+write.csv(df_mstmip_sg1_gpp, file = here("data/mstmip_sg1_gpp_glob.csv"))
+
+df_mstmip_sg1_gpp_long <- df_mstmip_sg1_gpp |>
+  pivot_longer(cols= -year,
+               names_to = "Model",
+               values_to = "GPP")
+
+for(i in 1:2) {
+  p_mstmip_sg1_gpp <- df_mstmip_sg1_gpp_long |>
+    ggplot(mapping= aes(x= GPP, y= ..density..)) +
+    geom_histogram(color= "lightgrey",fill= "darkgreen", binwidth = 1, size= 0.2) +
+    geom_boxplot(aes(y = -0.02),
+                 width = 0.01,
+                 alpha = 0.3,
+                 coef= 0,
+                 outlier.shape= NA,
+                 size= 0.3)+
+    labs(x= expression(GPP~(PgC~yr^{-1})),
+         y= "Density") +
+    theme_bw() +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.3, "cm"),
+      strip.text.x = element_text(size = 8),
+      strip.background= element_blank(),
+      strip.text = element_text(face = "bold")
+    ) +
+    facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
+  ggsave(sprintf(here("figures/distribution_mstmip_sg1_gpp_%01d.png"), i), plot = p_mstmip_sg1_gpp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+
+}
+
+
+### NBP:
+mstmip_sg1_nbp_files <- list.files(path= here("../data/mstmip/SG1/ann_glob/as_txt/"), pattern = "_nbp_.*\\.txt$")
+
+df_mstmip_sg1_nbp = data.frame("year" = 1982:2010)
+
+for (file in mstmip_sg1_nbp_files) {
+  mod_name <- sub("_nbp_ann_glob.txt", "", file)
+  mod <- read.table(paste("../data/mstmip/SG1/ann_glob/as_txt/", file, sep= "/"), quote= "\"")
+  colnames(mod) <- c("year", mod_name)
+
+  mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
+  df_mstmip_sg1_nbp <- merge(df_mstmip_sg1_nbp, mod, by = "year", all.x = TRUE)
+}
+
+write.csv(df_mstmip_sg1_nbp, file = here("data/mstmip_sg1_nbp_glob.csv"))
+
+df_mstmip_sg1_nbp_long <- df_mstmip_sg1_nbp |>
+  pivot_longer(cols= -year,
+               names_to = "Model",
+               values_to = "NBP")
+
+for(i in 1:2) {
+  p_mstmip_sg1_nbp <- df_mstmip_sg1_nbp_long |>
+    ggplot(mapping= aes(x= NBP, y= ..density..)) +
+    geom_histogram(color= "lightgrey",fill= "blue", binwidth = 1, size= 0.2) +
+    geom_boxplot(aes(y = -0.02),
+                 width = 0.01,
+                 alpha = 0.3,
+                 coef= 0,
+                 outlier.shape= NA,
+                 size= 0.3)+
+    labs(x= expression(NBP~(PgC~yr^{-1})),
+         y= "Density") +
+    theme_bw() +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.3, "cm"),
+      strip.text.x = element_text(size = 8),
+      strip.background= element_blank(),
+      strip.text = element_text(face = "bold")
+    ) +
+    facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
+  ggsave(sprintf(here("figures/distribution_mstmip_sg1_nbp_%01d.png"), i), plot = p_mstmip_sg1_nbp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+
+}
+
+
+## SG3
+### GPP:
+mstmip_sg3_gpp_files <- list.files(path= here("../data/mstmip/SG3/ann_glob/as_txt/"), pattern = "_gpp_.*\\.txt$")
+
+df_mstmip_sg3_gpp = data.frame("year" = 1982:2010)
+
+for (file in mstmip_sg3_gpp_files) {
+  mod_name <- sub("_gpp_ann_glob.txt", "", file)
+  mod <- read.table(paste("../data/mstmip/SG3/ann_glob/as_txt/", file, sep= "/"), quote= "\"")
+  colnames(mod) <- c("year", mod_name)
+
+  mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
+  df_mstmip_sg3_gpp <- merge(df_mstmip_sg3_gpp, mod, by = "year", all.x = TRUE)
+}
+
+write.csv(df_mstmip_sg3_gpp, file = here("data/mstmip_sg3_gpp_glob.csv"))
+
+df_mstmip_sg3_gpp_long <- df_mstmip_sg3_gpp |>
+  pivot_longer(cols= -year,
+               names_to = "Model",
+               values_to = "GPP")
+
+for(i in 1:2) {
+  p_mstmip_sg3_gpp <- df_mstmip_sg3_gpp_long |>
+    ggplot(mapping= aes(x= GPP, y= ..density..)) +
+    geom_histogram(color= "lightgrey",fill= "darkgreen", binwidth = 1, size= 0.2) +
+    geom_boxplot(aes(y = -0.02),
+                 width = 0.01,
+                 alpha = 0.3,
+                 coef= 0,
+                 outlier.shape= NA,
+                 size= 0.3)+
+    labs(x= expression(GPP~(PgC~yr^{-1})),
+         y= "Density") +
+    theme_bw() +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.3, "cm"),
+      strip.text.x = element_text(size = 8),
+      strip.background= element_blank(),
+      strip.text = element_text(face = "bold")
+    ) +
+    facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
+  ggsave(sprintf(here("figures/distribution_mstmip_sg3_gpp_%01d.png"), i), plot = p_mstmip_sg3_gpp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+
+}
+
+
+### NBP:
+mstmip_sg3_nbp_files <- list.files(path= here("../data/mstmip/SG3/ann_glob/as_txt/"), pattern = "_nbp_.*\\.txt$")
+
+df_mstmip_sg3_nbp = data.frame("year" = 1982:2010)
+
+for (file in mstmip_sg3_nbp_files) {
+  mod_name <- sub("_nbp_ann_glob.txt", "", file)
+  mod <- read.table(paste("../data/mstmip/SG3/ann_glob/as_txt/", file, sep= "/"), quote= "\"")
+  colnames(mod) <- c("year", mod_name)
+
+  mod$year <- as.integer(year(format(as.Date(mod$year, format = "%Y-%m-%d"))))
+  df_mstmip_sg3_nbp <- merge(df_mstmip_sg3_nbp, mod, by = "year", all.x = TRUE)
+}
+
+write.csv(df_mstmip_sg3_nbp, file = here("data/mstmip_sg3_nbp_glob.csv"))
+
+df_mstmip_sg3_nbp_long <- df_mstmip_sg3_nbp |>
+  pivot_longer(cols= -year,
+               names_to = "Model",
+               values_to = "NBP")
+
+for(i in 1) {
+  p_mstmip_sg3_nbp <- df_mstmip_sg3_nbp_long |>
+    ggplot(mapping= aes(x= NBP, y= ..density..)) +
+    geom_histogram(color= "lightgrey",fill= "blue", binwidth = 1, size= 0.2) +
+    geom_boxplot(aes(y = -0.02),
+                 width = 0.01,
+                 alpha = 0.3,
+                 coef= 0,
+                 outlier.shape= NA,
+                 size= 0.3)+
+    labs(x= expression(NBP~(PgC~yr^{-1})),
+         y= "Density") +
+    theme_bw() +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.3, "cm"),
+      strip.text.x = element_text(size = 8),
+      strip.background= element_blank(),
+      strip.text = element_text(face = "bold")
+    ) +
+    facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
+  ggsave(sprintf(here("figures/distribution_mstmip_sg3_nbp_%01d.png"), i), plot = p_mstmip_sg3_nbp, width = 8.27, height = 11.69, units= "in", dpi = 300)
+
+}
+
+
+GPP-Models
+## P-Model
+pMod_gpp_glob <- read_table(here("data/pMod_gpp_glob.txt"), col_names= c("Year", "GPP"), skip = 1)
+
+
+## FLUXCOM
+fluxcom_gpp_glob <- read_table(here("data/fluxcom_gpp_glob.txt"), col_names= c("Year", "GPP"), skip = 1)
+
+
+
+Steps:
+
+  1. Create shared table df
+  2. create Column Model
+  3. do gpp Plot
+# ............................
+
+for(i in 1) {
+  p_pMod_gpp_glob <- pMod_gpp_glob |>
+    ggplot(mapping= aes(x= GPP, y= ..density..)) +
+    geom_histogram(color= "lightgrey",fill= "darkgreen", binwidth = 1, size= 0.2) +
+    geom_boxplot(aes(y = -0.02),
+                 width = 0.01,
+                 alpha = 0.3,
+                 coef= 0,
+                 outlier.shape= NA,
+                 size= 0.3)+
+    labs(x= expression(GPP~(PgC~yr^{-1})),
+         y= "Density") +
+    theme_bw() +
+    theme(
+      legend.position="none",
+      panel.spacing = unit(0.3, "cm"),
+      strip.text.x = element_text(size = 8),
+      strip.background= element_blank(),
+      strip.text = element_text(face = "bold")
+    ) +
+    facet_wrap_paginate(~Model, ncol= 3, nrow= 3, page= i)
+  ggsave(sprintf(here("figures/distribution_pMod_gpp_%01d.png"), i), plot = pMod_gpp_glob, width = 8.27, height = 11.69, units= "in", dpi = 300)
+
+}
