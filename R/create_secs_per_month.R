a60db3a935bc986650cbdf4f7c66f04375fd6c5a
TrinidadTimo <timo.trinidad@gmail.com>
2025-07-26 09:50:23 +0200

    Reprocessed time axis of LPJ-GUESS and LPJmL.

--- R/create_secs_per_month.R
+++ R/create_secs_per_month.R
@@ -11,27 +11,24 @@ library(lubridate)
 
 
 # get directions
-dirin <- "/data_2/scratch/ttrinidad/data/trendy/S2/raw/"
-dirout <- "/data_2/scratch/ttrinidad/data/trendy/S2/secs_per_month/"
+dirin <- "/data_2/scratch/ttrinidad/data/trendy/S3/raw/"
+dirout <- "/data_2/scratch/ttrinidad/data/trendy/S3/secs_per_month/"
 
-filnam <- paste0(dirin, "CLASSIC_S2_gpp.nc")
+filnam <- paste0(dirin, "LPJ-GUESS_S3_gpp.nc")
 
 # read monthly file
 ref <- rast(filnam)
 ref
-#crs(ref) <- "EPSG:4326"
 
 length(time(ref))
 
-tvec <- time(ref)
-
-#length_out = 240
+tvec <- time(ref) # -> clean!
 
 # if not available create it yourself
 tvec_clean <- seq(from = ymd("1701-01-01"), by = "month", length.out = length(tvec)) # length.out = length_out
 
 # Compute days per month for each date
-days_per_month <- days_in_month(tvec_clean) #choose tvec_clean or t_vec depending on validity of time axis (however create both anyways for below)!!
+days_per_month <- days_in_month(tvec) #choose tvec_clean or t_vec depending on validity of time axis (however create both anyways for below)!!
 
 # Convert to seconds
 seconds_per_month <- days_per_month * 86400  # 86400 seconds/day
@@ -42,12 +39,14 @@ time_raster <- rast(rep(template, length(seconds_per_month)))
 time_raster[] <- rep(seconds_per_month, each = ncell(template))
 
 # Set time dimension
-time(time_raster) <- tvec_clean
-names(time_raster) <- paste0("t", seq_along(tvec_clean))
+time(time_raster) <- tvec
+names(time_raster) <- paste0("t", seq_along(tvec))
 
 writeCDF(
   time_raster,
-  file.path(dirout,"seconds_per_month.nc"),
+  file.path(dirout,"LPJ-GUESS_seconds_per_month.nc"),
   varname = "seconds_per_month",
   overwrite = TRUE
   )
+
+
