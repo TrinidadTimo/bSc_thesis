35e48f3894d1ba1ce36f3525c9f23269a5b99943
TrinidadTimo <timo.trinidad@gmail.com>
2025-08-20 09:47:26 +0200

    Added lines for calculating coefficient of variation.

--- vignettes/process_sdgvm_nbp.R
+++ vignettes/process_sdgvm_nbp.R
@@ -13,3 +13,12 @@ write_csv(glob_detr, here("../data/trendy/S3/ann_glob_detr/R/SDGVM_S3_nbp_ANN_GL
 glob_detr_iav <- tibble("IAV_NBP" = var(glob_detr$NBP))
 write_csv(glob_detr_iav, here("../data/trendy/S3/iavar/R/SDGVM_S3_nbp_VAR_GLOB.csv"))
 
+
+## Coefficient of Variability
+glob_gpp <- read_table("/data_2/scratch/ttrinidad/data/trendy/S3/ann_glob_absolute/R/as_txt/SDGVM_S3_gpp_GLOB_ABSOLUTE.txt")[,1:2]
+colnames(glob_gpp) <- c("year", "gpp")
+
+glob_detr_var_coef <- as.numeric(glob_detr_iav/mean(glob_gpp$gpp))
+df_glob_detr_var_coef <- tibble("VAR_COEF" = glob_detr_var_coef)
+
+write_csv(df_glob_detr_var_coef, here("../data/trendy/S3/var_coef/SDGVM_S3_nbp_var_coef.csv"))
